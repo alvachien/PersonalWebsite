7b9942e33ceb07074c263707765468b981f5e8a3
Alva Chien <alvachien@live.com>
2020-10-07 22:51:59 +0800

    Add new codes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
-import React from "react"
+import React from "react";
+import Header from '../components/header';
+import Parallax from '../components/parallax';
+import Container from '../components/container';
+import Footer from '../components/footer';
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
-    </div>
+    <div>
+      <Header title="alvachien.com"></Header>
+      <Parallax title="Your title" image="bg2.jpg"></Parallax>
+      <Container>
+      <div style={{ color: `teal` }}>
+        <h1>About</h1>
+        <p>This is an About page.</p>
+      </div>
+      </Container>
+      <Footer />
+    </div>    
   )
 }
